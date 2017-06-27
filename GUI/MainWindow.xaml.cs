7677e9c9f5351363df71307d445b557bc3c05554
Anton Golubkov <AntGolubkov@gmail.com>
2017-06-27 17:02:33 +0300

    GUI namespace to ClashOfTanks.GUI namespace.

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace ClashOfTanks.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
